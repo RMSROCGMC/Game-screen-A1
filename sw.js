bb6ee5ea2d877b04dfd34e521dac61a29703ff4f
RMSROCGMC <rmsgithub@gmail.com>
2026-05-10 01:13:23 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,14 @@
-const CACHE_NAME = 'warrior-history-v3';
-const ASSETS_TO_CACHE = [
-    './',
-    './index.html',
-    './game.js',
-    './1778345687994.png',
-    './manifest.json'
-];
+const CACHE_NAME = 'pure-code-v5';
+const ASSETS = ['./', './index.html', './game.js', './manifest.json'];
 
 self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE)));
+    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
     self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-    e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null))));
+    e.waitUntil(caches.keys().then(ks => Promise.all(ks.map(k => k !== CACHE_NAME && caches.delete(k)))));
     self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
