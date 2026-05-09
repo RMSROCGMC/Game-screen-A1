07820ee5e0538650d60266d83dbf0b6235ec49a2
RMSROCGMC <rmsgithub@gmail.com>
2026-05-10 02:39:31 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,10 @@
-const CACHE_NAME = 'tkms-v6';
-const ASSETS = [
-  './',
-  './index.html',
-  './game.js',
-  './questions.js',
-  './manifest.json',
-  './logo.jpeg'
-];
-
-// 安裝並強制更新
+const CACHE_NAME = 'tkms-v9';
+const ASSETS = ['./', './index.html', './game.js', './questions.js', './manifest.json', './logo.jpeg'];
 self.addEventListener('install', (e) => {
-  self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+    self.skipWaiting();
+    e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
 });
-
-// 清除舊版緩存
 self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.map((key) => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      }));
-    })
-  );
+    e.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((k) => k !== CACHE_NAME && caches.delete(k)))));
 });
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
-});
+self.addEventListener('fetch', (e) => e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request))));
