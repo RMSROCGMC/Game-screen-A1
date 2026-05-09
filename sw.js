df297e7517ccc9916149a484c051b06d893d5b14
RMSROCGMC <rmsgithub@gmail.com>
2026-05-10 02:29:02 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,34 @@
-const CACHE_NAME = 'tkms-v5';
-const ASSETS = ['./', './index.html', './game.js', './questions.js', './manifest.json', './logo.jpeg'];
+const CACHE_NAME = 'tkms-v6';
+const ASSETS = [
+  './',
+  './index.html',
+  './game.js',
+  './questions.js',
+  './manifest.json',
+  './logo.jpeg'
+];
 
+// 安裝並強制更新
 self.addEventListener('install', (e) => {
   self.skipWaiting();
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// 清除舊版緩存
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }));
+    })
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
 });
